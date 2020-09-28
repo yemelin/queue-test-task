bac9992228c523e55418725ba83a02d8a7bbfaa8
Vasilii Emelin <yemelin@gmail.com>
2020-09-28 12:49:30 +0300

    fix test

--- generator_test.go
+++ generator_test.go
@@ -3,12 +3,15 @@
 package main
 
 import (
+	"context"
+	"sync"
 	"testing"
 	"time"
 )
 
-func createGen() *Generator {
+func createGen(ctx context.Context) *Generator {
 	return &Generator{
+		parentctx:  ctx,
 		timeout:    1000 * time.Millisecond,
 		sendPeriod: 100 * time.Millisecond,
 		out:        make(chan Data),
@@ -17,28 +20,32 @@ func createGen() *Generator {
 			{ID: "data_1", InitValue: 10, MaxChangeStep: 10},
 			{ID: "data_2", InitValue: 10, MaxChangeStep: 1},
 		},
+		logger: NewLogger("TestGen"),
 	}
 }
 
-func TestSimple(t *testing.T) {
-
+func TestGenerator(t *testing.T) {
 	t.Run("should be stopped by force", func(t *testing.T) {
-		g := createGen()
+		ctx := context.Background()
+		g := createGen(ctx)
 		var stoppedByForce bool
 		go func() {
 			time.Sleep(200 * time.Millisecond)
 			g.Stop()
 			stoppedByForce = true
 		}()
-		_, done := g.Start()
-		<-done
+		var anyWg sync.WaitGroup
+		anyWg.Add(1)
+		g.Start(&anyWg)
+		anyWg.Wait()
 		if !stoppedByForce {
 			t.Fatalf("not stopped by force")
 		}
 	})
 
 	t.Run("should not block sending (part of the values is read)", func(t *testing.T) {
-		g := createGen()
+		ctx := context.Background()
+		g := createGen(ctx)
 		var receiver []Data
 		var stoppedByForce bool
 		go func() {
@@ -46,18 +53,22 @@ func TestSimple(t *testing.T) {
 			g.Stop()
 			stoppedByForce = true
 		}()
-		in, done := g.Start()
+		var anyWg sync.WaitGroup
+		in := g.Outbound()
+		anyWg.Add(1)
+		g.Start(&anyWg)
 		for i := 0; i < 5; i++ {
 			receiver = append(receiver, <-in)
 		}
-		<-done
+		anyWg.Wait()
 		if stoppedByForce {
 			t.Fatalf("should stop by itself")
 		}
 	})
 
 	t.Run("should not block sending (receiver sleeps)", func(t *testing.T) {
-		g := createGen()
+		ctx := context.Background()
+		g := createGen(ctx)
 		var receiver []Data
 		var stoppedByForce bool
 		go func() {
@@ -65,19 +76,25 @@ func TestSimple(t *testing.T) {
 			g.Stop()
 			stoppedByForce = true
 		}()
-		in, done := g.Start()
+
+		var anyWg sync.WaitGroup
+		in := g.Outbound()
+		anyWg.Add(1)
+		g.Start(&anyWg)
 		for v := range in {
 			receiver = append(receiver, v)
 			time.Sleep(200 * time.Millisecond)
 		}
-		<-done
+		
+		anyWg.Wait()
 		if stoppedByForce {
 			t.Fatalf("should stop by itself")
 		}
 	})
 
 	t.Run("should read all values)", func(t *testing.T) {
-		g := createGen()
+		ctx := context.Background()
+		g := createGen(ctx)
 		var receiver []Data
 		var stoppedByForce bool
 		go func() {
@@ -85,11 +102,14 @@ func TestSimple(t *testing.T) {
 			g.Stop()
 			stoppedByForce = true
 		}()
-		in, done := g.Start()
+		var anyWg sync.WaitGroup
+		in := g.Outbound()
+		anyWg.Add(1)
+		g.Start(&anyWg)
 		for v := range in {
 			receiver = append(receiver, v)
 		}
-		<-done
+		anyWg.Wait()
 		if stoppedByForce {
 			t.Fatalf("should stop by itself")
 		}
